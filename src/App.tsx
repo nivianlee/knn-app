80953992583b64ccbbe06b58b81959ba19d083f1
kwokkiejh <kwokkiejiahui@gmail.com>
2020-10-18 14:14:01 +0800

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TodoList from "./Container/TodoList";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const themeLight = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: "#ff4400",
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    background: {
+      default: "#fff5d7",
+    },
+  },
+  overrides: {
+    MuiAppBar: {
+      colorPrimary: {
+        backgroundColor: "ffffff",
+        color: "000000",
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MuiThemeProvider theme={themeLight}>
+        <CssBaseline />
+
+        <TodoList />
+      </MuiThemeProvider>
     </div>
   );
 }
---
+++ src/Component/TodoItem.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const TodoItem = () => {
+  return <div />;
+};
+
+export default TodoItem;
